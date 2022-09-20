059e795175d7bb3985c4a5a48745667a8f8a61cf
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2022-09-20 09:49:36 -0500

    Add initial questions

--- Gpa.java
+++ Gpa.java
@@ -1,11 +1,24 @@
-java.text.DecimalFormat;
-javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
 
 public class Gpa{
 
 	public static void main(String[] args){
 		
+		DecimalFormat formatter = new DecimalFormat("#.00");
 		
+		int totalClasses = Integer.parseInt(JOptionPane.showInputDialog(null, "How many classes are you taking?", "Gpa calculator", JOptionPane.QUESTION_MESSAGE));
+		
+		char[] grades;
+		int[] creditHours;
+		
+		int i = 0;
+		while (i < totalClasses){
+			
+			String gradeTemp = (JOptionPane.showInputDialog(null, "What is your grade for class " + (i+1) + "?", "Gpa calculator", JOptionPane.QUESTION_MESSAGE)).toUpperCase();
+
+			i++;
+		}
 		
 	}
 }
